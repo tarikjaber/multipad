5d9d0563f61bc64231f4de455795f0fcec8de0b9
Tarik Jaber <tariknjaber@gmail.com>
2023-06-19 09:19:07 -0400

    Save top row to local storage

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Editor from '@monaco-editor/react';
 import { Box, InputLabel, Select, MenuItem, FormControl, Tooltip, IconButton, SelectChangeEvent } from '@mui/material';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
@@ -14,23 +14,37 @@ interface BodyProps {
 function Body({ toggleDarkMode, darkMode }: BodyProps) {
   const [numEditorRows, setNumEditorRows] = useState<number>(1);
   const [numEditorsPerRow, setNumEditorsPerRow] = useState<number>(1);
-  const [editorLanguage, setEditorLanguage] = useState<string>('csharp');
+  const [editorLanguage, setEditorLanguage] = useState<string>('markdown');
 
+  useEffect(() => {
+    setNumEditorRows(localStorage.getItem('numEditorRows') ? Number(localStorage.getItem('numEditorRows')) : 1);
+    setNumEditorsPerRow(localStorage.getItem('numEditorsPerRow') ? Number(localStorage.getItem('numEditorsPerRow')) : 1);
+    setEditorLanguage(localStorage.getItem('editorLanguage') || 'markdown');
+  }, []);
+
+  // Save settings and contents to local storage whenever they change
   const handleNumEditorRowsChange = (event: SelectChangeEvent) => {
     setNumEditorRows(Number(event.target.value));
+    localStorage.setItem('numEditorRows', event.target.value);
   };
 
   const handleNumEditorsPerRowChange = (event: SelectChangeEvent) => {
     setNumEditorsPerRow(Number(event.target.value));
+    localStorage.setItem('numEditorsPerRow', event.target.value);
   };
 
   const handleEditorLanguageChange = (event: SelectChangeEvent) => {
-    console.log("Language changed to: " + event.target.value);
+    localStorage.setItem('editorLanguage', event.target.value);
     setEditorLanguage(event.target.value);
   };
 
+  const handleEditorContentChange = (index: number, value: string) => {
+    const newContents = [];
+    newContents[index] = value;
+  };
+
   return (
-    <Stack sx={{ padding: '15px 10px 10px', display: 'flex', flexDirection: 'column', height: "100vh"}} spacing="10px" flexGrow={1}>
+    <Stack sx={{ padding: '15px 10px 10px', display: 'flex', flexDirection: 'column', height: "100vh" }} spacing="10px" flexGrow={1}>
       <Box sx={{ display: 'flex', gridGap: '10px' }}>
         <FormControl fullWidth size="small">
           <InputLabel id="num-editor-rows-label"># Editor Rows</InputLabel>
@@ -38,7 +52,6 @@ function Body({ toggleDarkMode, darkMode }: BodyProps) {
             labelId="num-editor-rows-label"
             id="num-editor-rows"
             value={numEditorRows.toString()}
-            defaultValue="1"
             onChange={handleNumEditorRowsChange}
             label="# Editor Rows"
           >
@@ -51,7 +64,6 @@ function Body({ toggleDarkMode, darkMode }: BodyProps) {
           <Select
             labelId="num-editors-per-row-label"
             id="num-editors-per-row"
-            defaultValue="1"
             value={numEditorsPerRow.toString()}
             onChange={handleNumEditorsPerRowChange}
             label="# Editors per Row"
@@ -65,15 +77,20 @@ function Body({ toggleDarkMode, darkMode }: BodyProps) {
           <Select
             labelId="editor-language-label"
             id="editor-language"
-            defaultValue='csharp'
             label="Language"
             value={editorLanguage}
             onChange={handleEditorLanguageChange}
           >
+            <MenuItem value={'markdown'}>Markdown</MenuItem>
             <MenuItem value={'javascript'}>JavaScript</MenuItem>
+            <MenuItem value={'typescript'}>TypeScript</MenuItem>
             <MenuItem value={'csharp'}>C#</MenuItem>
+            <MenuItem value={'java'}>Java</MenuItem>
+            <MenuItem value={'cpp'}>C++</MenuItem>
             <MenuItem value={'rust'}>Rust</MenuItem>
             <MenuItem value={'python'}>Python</MenuItem>
+            <MenuItem value={'ruby'}>Ruby</MenuItem>
+            <MenuItem value={'html'}>HTML</MenuItem>
           </Select>
         </FormControl>
         <Tooltip title={darkMode ? "Switch to light mode" : "Switch to dark mode"}>
@@ -89,10 +106,12 @@ function Body({ toggleDarkMode, darkMode }: BodyProps) {
               <Grid2 key={`editor-${i * Number(numEditorsPerRow) + j}`} sx={{ height: '100%', border: 1, borderColor: 'grey.500' }} width={`${numEditorsPerRow === 2 ? "50%" : "100%"}`} display="inline-block">
                 <Editor
                   options={{
-                    minimap: { enabled: false }, automaticLayout: true, glyphMargin: false, lineNumbersMinChars: 2, lineDecorationsWidth: 5 }}
+                    minimap: { enabled: false }, automaticLayout: true, glyphMargin: false, lineNumbersMinChars: 2, lineDecorationsWidth: 5
+                  }}
                   language={editorLanguage}
-                  defaultValue="// some comment"
+                  //value={editorContents[i * numEditorsPerRow + j] || ''}
                   theme={darkMode ? 'vs-dark' : 'vs-light'}
+                  onChange={(value) => handleEditorContentChange(i * numEditorsPerRow + j, value || '')}
                 />
               </Grid2>
             ))}
