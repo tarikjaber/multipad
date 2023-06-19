587187544f77b37b4dba39ea98af11630cb909d3
Tarik Jaber <tariknjaber@gmail.com>
2023-06-18 23:00:46 -0400

    Fix warnings 2

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Editor from '@monaco-editor/react';
-import { Box, InputLabel, Select, MenuItem, FormControl, Paper, Tooltip, IconButton, SelectChangeEvent } from '@mui/material';
+import { Box, InputLabel, Select, MenuItem, FormControl, Tooltip, IconButton, SelectChangeEvent } from '@mui/material';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import Stack from '@mui/material/Stack';
@@ -86,7 +86,7 @@ function Body({ toggleDarkMode, darkMode }: BodyProps) {
         {Array.from({ length: Number(numEditorRows) }).map((_, i) =>
           <Grid2 sx={{ flex: 1 }} height={`${numEditorsPerRow === 2 ? "50%" : "100%"}`}>
             {Array.from({ length: Number(numEditorsPerRow) }).map((_, j) => (
-              <Grid2 key={`editor-${i * Number(numEditorsPerRow) + j}`} sx={{ height: '100%', border: 1, borderColor: 'grey.500' }} width={`${numEditorsPerRow ==- 2 ? "50%" : "100%"}`} display="inline-block">
+              <Grid2 key={`editor-${i * Number(numEditorsPerRow) + j}`} sx={{ height: '100%', border: 1, borderColor: 'grey.500' }} width={`${numEditorsPerRow === 2 ? "50%" : "100%"}`} display="inline-block">
                 <Editor
                   options={{
                     minimap: { enabled: false }, automaticLayout: true, glyphMargin: false, lineNumbersMinChars: 2, lineDecorationsWidth: 5 }}
